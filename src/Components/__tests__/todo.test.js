6b75bc21536cc6f3a1c9807a32300cb151c9959e
Gaurav <gaurav.jain2@soprasteria.com>
2021-08-13 17:55:15 +0530

    Advance Testing

--- src/Components/__tests__/todo.test.js
+++ src/Components/__tests__/todo.test.js
@@ -3,9 +3,38 @@ import Todo from "../todo";
 // we will need this file in order to test it
 // we will find the id from todo.js into the todo.test.js file and render it 
 
-test('should render todo component', ()=>
+afterEach (() => {
+    cleanup();
+});
+
+test('should render completed todo ',()=>
 {
-    render(<Todo/>);
+    const todo = {id:1, title: "Bring Grocery", completed : false};
+    render (<Todo todo = {todo} />);
     const todoElement = screen.getByTestId('todo-1');
     expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('Bring Grocery');
+    expect(todoElement).toContainElement('<strike>'); 
 })
+
+test('should render not-completed todo',()=>
+{
+    const todo = {id:2, title: "Wash Utensils", completed : true};
+    render (<Todo todo = {todo} />);
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('Wash Utensils');
+    expect(todoElement).toContainElement('<strike>'); 
+})
+
+afterEach (() => {
+    cleanup();
+});
+
+// MCQ -> Command 
+//redux initalixze json server 
+// API -> JSON 
+// C- Create
+// R- Read 
+// U - Update
+// D - Delete
